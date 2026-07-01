f9b133ff87e89eb042b2716d014e52b660afaa97
Camila Aballay <aballaycamila4@gmail.com>
2026-07-01 18:59:24 -0300

    Agregación de fotos a carrusel flora rebelde

---
+++ js/flora.js
@@ -0,0 +1,39 @@
+const imagenes = [
+    "img/f1.jpg",
+    "img/f2.jpg",
+    "img/f3.jpg",
+    "img/f4.jpg",
+    "img/f5.jpg",
+    "img/f6.jpg"
+];
+
+let indice = 0;
+
+const slide = document.getElementById("slide");
+
+function mostrarImagen() {
+    slide.src = imagenes[indice];
+}
+
+function siguiente() {
+    indice++;
+
+    if (indice >= imagenes.length) {
+        indice = 0;
+    }
+
+    mostrarImagen();
+}
+
+function anterior() {
+    indice--;
+
+    if (indice < 0) {
+        indice = imagenes.length - 1;
+    }
+
+    mostrarImagen();
+}
+
+// Cambia automáticamente cada 4 segundos
+setInterval(siguiente, 4000);
